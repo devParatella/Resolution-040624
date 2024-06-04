48038cd843b2fc2e95c66cab577fa5b4f7c98d98
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-04 16:51:00 -0300

    Exemplo de variáveis mutáveis em Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+// Esta é a função principal do programa.
+// Ela demonstra o uso de variáveis mutáveis, literais de string e a macro println!.
 fn main() {
-    let _name: &str = "Marcos";
+    // Uma referência mutável a um literal de string representando o nome.
+    // Ela é inicializada com o valor "Marcos" e depois atualizada para "Paratella".
+    let mut _name: &str = "Marcos";
+    _name = "Paratella";
+
+    // Um inteiro sem sinal de 32 bits representando a idade.
+    // Ele é inicializado com o valor 25.
+    let _age: u32 = 25;
+
+    // Imprime uma mensagem de saudação no console, usando o valor da variável `_name`.
     println!("Hello, {}!", _name);
 }
+
