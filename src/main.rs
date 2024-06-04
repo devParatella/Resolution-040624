be3c2c753482d4286634fe4a745c171546c98b0b
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-04 17:01:55 -0300

    _ Ignora valores não utilizados conscientemente.

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,31 @@ fn main() {
 
     // Imprime uma mensagem de saudação no console, usando o valor da variável `_name`.
     println!("Hello, {}!", _name);
-}
+
+    //Em Rust, o uso de underscores (_) em variáveis tem algumas convenções e
+    // significados específicos. Aqui estão algumas das principais utilizações:
+    let (_x, _) = (5, 10);
+    // Aqui, estamos ignorando o segundo valor (10).
+
+    //Prefixo de Variáveis Não Utilizadas: Se você declarar uma variável mas 
+    //não for utilizá-la, você pode prefixá-la com _ para evitar warnings de 
+    //variáveis não utilizadas.
+    let _unused_variable: i32 = 42;
+    // Isto informa ao compilador que você sabe que a variável não será usada.
+
+    //...
+    let some_value: i32 = 5; // Define the variable `some_value`
+    
+    //Subdivisão de Padrões em match: Dentro de um bloco match, _ pode ser 
+    //usado para corresponder a qualquer valor, funcionando como um 
+    //"correspondente padrão".
+    match some_value {
+        1 => println!("Um"),
+        2 => println!("Dois"),
+        _ => println!("Outro valor"),
+    }
+    // O "_" aqui corresponderá a qualquer valor que não seja 1 ou 2.
+
+    }
+
 
